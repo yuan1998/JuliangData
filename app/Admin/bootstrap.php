c97d42168a4a53969f3fbbf9b8a789199f0447f4
yuan1998 <chizhiyueshu@gmail.com>
2020-06-09 09:25:02 +0800

    fix ELement-UI CDN Url

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -24,7 +24,7 @@ Admin::js('/js/app.js');
 Admin::css('//cdn.jsdelivr.net/npm/element-ui@2.12.0/lib/theme-chalk/index.css');
 //Admin::headerJs('//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js');
 Admin::js('https://cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js');
-Admin::js('//unpkg.com/element-ui/lib/index.js');
+Admin::js('https://cdn.jsdelivr.net/npm/element-ui@2.13.2/lib/index.js');
 Admin::css('/css/app.css');
 
 Encore\Admin\Form::forget(['map', 'editor']);
