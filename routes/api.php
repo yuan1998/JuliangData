8f068280020422da1e0148d6e46adcc2000a2f28
刘云 <liuyun@liuyundeMac-mini.local>
2021-12-01 16:21:16 +0800

    提交快手测试

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::prefix('v1')
 
         Route::prefix('club')->name('club.')->group(function () {
             Route::post('post', 'ClubController@post');
+            Route::post('ks_post', 'ClubController@ksPost');
             Route::post('baiduPost', 'ClubController@baiduPost');
             Route::post('customerPost', 'ClubController@customerPost');
         });
